f18feadc31fcbd1278a2e33a399b317b169aa3b2
sachinkr0872 <sachinkr0872@gmail.com>
2026-01-05 00:15:22 +0530

    feat: check type of user

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-// Object Literals 
+// Object Literals
 
 let user = {
-    name: 'crystal',
-    age: 30 ,
-    email:'crystal@exmaple.com' ,
-    location: 'berlin',
-    blogs:['why mac & cheese rules', '10 things to make with marmite']
-
+  name: "crystal",
+  age: 30,
+  email: "crystal@exmaple.com",
+  location: "berlin",
+  blogs: ["why mac & cheese rules", "10 things to make with marmite"],
 };
 
 console.log(user);
 console.log(user.name);
 
-// overwrite age 
+// overwrite age
 
-user.age = 35
-console.log(user.age)
+// user.age = 35
+console.log(user.age);
+
+//we can also use sqyare brackets instead of dot notation 
+console.log(user["email"]);
+
+user["name"] = "sachin";
+console.log(user["name"]);
+
+//checking the type of user
+console.log(typeof user)
