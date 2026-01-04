9d4b4ade0383cc7f3e59cf03d16fa1e936e3fcf5
sachinkr0872 <sachinkr0872@gmail.com>
2026-01-04 23:43:44 +0530

    feat: add user object

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // Object Literals 
 
-let user = 
+let user = {
+    name: 'crystal',
+    age: 30 ,
+    email:'crystal@exmaple.com' ,
+    location: 'berlin',
+    blogs:['why mac & cheese rules', '10 things to make with marmite']
+
+};
+
+console.log(user);
