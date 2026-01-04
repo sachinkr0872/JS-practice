dfed8dd2a89456a2aeeea20c899e5b3e736c15c1
sachinkr0872 <sachinkr0872@gmail.com>
2026-01-05 00:27:04 +0530

    feat: learn about methods

--- script.js
+++ script.js
@@ -6,21 +6,35 @@ let user = {
   email: "crystal@exmaple.com",
   location: "berlin",
   blogs: ["why mac & cheese rules", "10 things to make with marmite"],
+  //   method login created
+  login: function () {
+    console.log("the user logged in");
+  },
+
+  logout: function () {
+    console.log('the user logged out')
+  }
 };
 
-console.log(user);
-console.log(user.name);
+user.login();
+user.logout()
+
+const name = "sachin";
+name.toUpperCase();
+
+// console.log(user);
+// console.log(user.name);
 
 // overwrite age
 
 // user.age = 35
-console.log(user.age);
+// console.log(user.age);
 
-//we can also use sqyare brackets instead of dot notation 
-console.log(user["email"]);
+//we can also use sqyare brackets instead of dot notation
+// console.log(user["email"]);
 
-user["name"] = "sachin";
-console.log(user["name"]);
+// user["name"] = "sachin";
+// console.log(user["name"]);
 
 //checking the type of user
-console.log(typeof user)
+// console.log(typeof user)
