9dbd82c3193e7304b47dfe5b6e687b9b51b8f23f
sachinkr0872 <sachinkr0872@gmail.com>
2026-01-09 19:59:51 +0530

    learn about math object

--- FE-02-01/script.js
+++ FE-02-01/script.js
@@ -1,22 +1,40 @@
-// ---------------=-----Object Literals----------------------//
+// --------------------Object Literals----------------------//
+
+// ---------Math Object-----------
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 7.7;
+
+console.log(Math.round(area));      // round method 
+console.log(Math.floor(area));      // floor method 
+console.log(Math.ceil(area));      // ceil method 
+console.log(Math.trunc(area));      // trunc method 
 
 
-// array of object
-const blogs = [
-  { title: "why mac & cheese rules", likes: 30 },
-  { title: "10 things to make with marmite", likes: 50 },
-];
 
-console.log(blogs);
+
+// ---------array of object---------
+// const blogs = [
+//   { title: "why mac & cheese rules", likes: 30 },
+//   { title: "10 things to make with marmite", likes: 50 },
+// ];
+
+// console.log(blogs);
 
 // let user = {
 //   name: "crystal",
 //   age: 30,
 //   email: "crystal@exmaple.com",
 //   location: "berlin",
-//   blogs: ["why mac & cheese rules", "10 things to make with marmite"],
-//    method login created
-//    login(){}  this is also a regular function
+//   blogs: [
+//     { title: "why mac & cheese rules", likes: 30 },
+//   { title: "10 things to make with marmite", likes: 50 },
+//   ],
+//     //   --------  method login created
+//  //  -------  login(){}  this is also a regular function
 //   login: function () {
 //     console.log("the user logged in");
 //   },
@@ -24,13 +42,13 @@ console.log(blogs);
 //   logout: function () {
 //     console.log("the user logged out");
 //   },
-//  using --this-- keyword
+// //     ------- using --this-- keyword
 //   logBlogs: function(){
 // console.log(this.blogs);
 //       console.log('this user has written the following blogs')
 //       this.blogs.forEach(blog => {
-//         console.log(blog)
-//       })
+//         console.log(blog.title, blog.likes);
+//       });
 //   }
 // };
 
@@ -43,12 +61,12 @@ console.log(blogs);
 //     console.log(this.name);
 //   }
 // };
-// user.show(); // Sachin
+// user.show(); //--- Sachin
 
 // function test() {
 //   console.log(this);
 // }
-// test(); // window (non-strict)
+// test(); // ----window (non-strict)
 
 // user.login();
 // user.logout();
@@ -59,16 +77,16 @@ console.log(blogs);
 // console.log(user);
 // console.log(user.name);
 
-// overwrite age
+//---- overwrite age
 
 // user.age = 35
 // console.log(user.age);
 
-//we can also use sqyare brackets instead of dot notation
+//----we can also use square brackets instead of dot notation
 // console.log(user["email"]);
 
 // user["name"] = "sachin";
 // console.log(user["name"]);
 
-//checking the type of user
+//-----checking the type of user
 // console.log(typeof user)
