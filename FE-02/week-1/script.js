fe6a202f5b4a9fe74a99cb06c8eae5343c759e2f
sachinkr0872 <sachinkr0872@gmail.com>
2026-01-11 22:51:36 +0530

    feat: learn about Fetch API basics

--- FE-02/week-1/script.js
+++ FE-02/week-1/script.js
@@ -263,7 +263,9 @@
 // let entries = Object.entries(myObject);
 // console.log(entries[2][1]);
 
-// ------------------------------------------- Async JS (Asynchronous js overview) - Callbacks, Promises, Async Await   --------------------------------------------------------
+
+
+// -------------------------    Async JS (Asynchronous js overview) - Callbacks, Promises, Async Await ----------------------------------------------
 
 // ----------------------  Callbacks    -------------------
 
@@ -291,62 +293,66 @@
 
 // createPost({ title: "Post Three", body: "This is post three" }, getPosts);
 
+
 // -----------------   Promises  -------------------
 
-const posts = [
-  { title: "Post One", body: "This is post one" },
-  { title: "Post Two", body: "This is post two" },
-];
+// const posts = [
+//   { title: "Post One", body: "This is post one" },
+//   { title: "Post Two", body: "This is post two" },
+// ];
 
-function getPosts() {
-  setTimeout(() => {
-    let output = "";
-    posts.forEach((post, index) => {
-      output += `<li>${post.title}</li>`;
-    });
-    document.body.innerHTML = output;
-  }, 1000);
-}
+// function getPosts() {
+//   setTimeout(() => {
+//     let output = "";
+//     posts.forEach((post, index) => {
+//       output += `<li>${post.title}</li>`;
+//     });
+//     document.body.innerHTML = output;
+//   }, 1000);
+// }
 
-function createPost(post) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      posts.push(post);
+// function createPost(post) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       posts.push(post);
 
-      const error = false;
+//       const error = false;
 
-      if (!error) {
-        resolve();
-      } else {
-        reject("Error: Something went wrong");
-      }
-    }, 2000);
-  });
-}
+//       if (!error) {
+//         resolve();
+//       } else {
+//         reject("Error: Something went wrong");
+//       }
+//     }, 2000);
+//   });
+// }
 
 // createPost({ title: "Post Three", body: "This is post three" }).then(getPosts).catch(err => console.log(err))
 
+
 //-------------------------- Async / Await  -------------------------------
 
-async function init() {
-  await createPost({ title: "Post Three", body: "This is post three" });
+// async function init() {
+//   await createPost({ title: "Post Three", body: "This is post three" });
 
-  getPosts();
-}
+//   getPosts();
+// }
 
-init();
+// init();
+
 
 // ------------  Async / Await / Fetch --------------
 
-async function fetchUsers() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+// async function fetchUsers() {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/users");
 
-  const data = await res.json();
+//   const data = await res.json();
 
-  console.log(data);
-}
+//   console.log(data);
+// }
 
-fetchUsers();
+// fetchUsers();
+
 
 // -------Promise.all-------
 
@@ -359,3 +365,15 @@ fetchUsers();
 // const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
 
 //   Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values));
+
+
+
+
+
+
+//--------------------------------------------------------    Fetch API (application programming interface)    ------------------------------------------------------------------
+
+
+
+
+
