a2abdc7c6f550d9ca1877b7a22843ce92a459b15
sachinkr0872 <sachinkr0872@gmail.com>
2026-01-12 21:36:47 +0530

    learn about what is endpoints

--- FE-02/week-2/script.js
+++ FE-02/week-2/script.js
@@ -1,4 +1,13 @@
 // REST - Representational State Transfer(a http requests)
 // API - Application Programming Interface(messenger)
 
-// HTTP methods - 1. GET(retrieve data) , 2. POST(submit data), 3. PUT(update a specified resource) , 4. DELETE , 5. HEAD(same as GET but doesn't return a body) , 6. OPTIONS(returns the supported HTTP methods) 7. PATCH(update partial resources)
+// HTTP methods --- 1. GET(retrieve data) , 2. POST(submit data), 3. PUT(update a specified resource) , 4. DELETE , 5. HEAD(same as GET but doesn't return a body) , 6. OPTIONS(returns the supported HTTP methods) 7. PATCH(update partial resources)
+
+
+
+// Endpoints  --- GET (https://mysite.com/api/users) gives users data
+//            --- GET (https://mysite.com/api/users/1 or users/details/1) gives users details with id
+//            --- POST (https://mysite.com/api/users) add users to the server or the database
+//            --- PUT (https://mysite.com/api/users/1 or users/update/1) update the user data
+//            --- DELETE (https://mysite.com/api/users/1 or users/delete/1) delete the user data
+                  
