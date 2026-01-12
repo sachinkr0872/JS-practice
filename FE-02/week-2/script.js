449db1235b3728a50194baa085919563c3c2e6a2
sachinkr0872 <sachinkr0872@gmail.com>
2026-01-12 21:27:54 +0530

    learn about REST,API,HTTP methods

---
+++ FE-02/week-2/script.js
@@ -0,0 +1,4 @@
+// REST - Representational State Transfer(a http requests)
+// API - Application Programming Interface(messenger)
+
+// HTTP methods - 1. GET(retrieve data) , 2. POST(submit data), 3. PUT(update a specified resource) , 4. DELETE , 5. HEAD(same as GET but doesn't return a body) , 6. OPTIONS(returns the supported HTTP methods) 7. PATCH(update partial resources)
