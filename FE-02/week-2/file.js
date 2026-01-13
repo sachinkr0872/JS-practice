781752cd02bd597a3a5391505e1fdbe0591098e5
sachinkr0872 <sachinkr0872@gmail.com>
2026-01-13 22:37:39 +0530

    create text file using writeFileSync and writeFile

--- FE-02/week-2/file.js
+++ FE-02/week-2/file.js
@@ -1,9 +1,12 @@
 //---------------------------------------   File Handling in NodeJS    ------------------------------------------------
 
 // fs---file System
-const fs = require('fs');
+const fs = require("fs");
 
-
+// Sync......
 fs.writeFileSync('./test.txt', 'Hey sachin');
+
+// Async........
+fs.writeFile("./test.txt", "Hey sachin Async", (err) => {});
 
 //  git add FE-02/week-2/file.js
