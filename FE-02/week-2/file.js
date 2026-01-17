a93cd19c3cfc31252bd7bcd10d7227bcd102c7b1
sachinkr0872 <sachinkr0872@gmail.com>
2026-01-17 20:39:20 +0530

    learn : apeend files sync

--- FE-02/week-2/file.js
+++ FE-02/week-2/file.js
@@ -1,7 +1,7 @@
 //---------------------------------------   File Handling in NodeJS    ------------------------------------------------
 
 // fs---file System
-const fs = require("fs");
+// const fs = require("fs");
 
 // Sync......
 // fs.writeFileSync('./test.txt', 'Hey sachin');
@@ -12,12 +12,30 @@ const fs = require("fs");
 // const result = fs.readFileSync("./contacts.txt", "utf-8");
 // console.log(result);
 
-fs.readFile("./contacts.txt", "utf-8", (err, result) => {
-    if(err){
-        console.log("Error", err)
-    } else{
-        console.log(result)
-    }
-})
+// fs.readFile("./contacts.txt", "utf-8", (err, result) => {
+//     if(err){
+//         console.log("Error", err)
+//     } else{
+//         console.log(result)
+//     }
+// })
+
+const fs = require("fs");
+// const path = require("path");
+
+// const filePath = path.join(__dirname, "test.txt");
+
+// fs.appendFile(filePath, "Hello bhai\n", (err) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Data appended successfully");
+//     }
+// });
+
+
+fs.appendFileSync('./test.txt', new Date().getDate().toLocaleString())
+
+ fs.appendFileSync('./test.txt', 'Hey sachin');
 
 //  git add FE-02/week-2/file.js
---
+++ FE-02/week-2/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+// const path = require("path");
+
+// const filePath = path.join(__dirname, "test.txt");
+
+// fs.appendFile(filePath, "Hello bhai\n", (err) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Data appended successfully");
+//     }
+// });
