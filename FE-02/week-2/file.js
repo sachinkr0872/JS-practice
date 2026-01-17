775cbac1b0fcc2a9ef44aaa005cc47e0bcb4a16d
sachinkr0872 <sachinkr0872@gmail.com>
2026-01-17 20:15:24 +0530

    learn: read async file

--- FE-02/week-2/file.js
+++ FE-02/week-2/file.js
@@ -9,7 +9,15 @@ const fs = require("fs");
 // Async........
 // fs.writeFile("./test.txt", "Hey sachin Async", (err) => {});
 
-const result = fs.readFileSync("./contacts.txt", "utf-8");
-console.log(result);
+// const result = fs.readFileSync("./contacts.txt", "utf-8");
+// console.log(result);
+
+fs.readFile("./contacts.txt", "utf-8", (err, result) => {
+    if(err){
+        console.log("Error", err)
+    } else{
+        console.log(result)
+    }
+})
 
 //  git add FE-02/week-2/file.js
