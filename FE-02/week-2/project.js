f2540b1b2285a59ac248e989891bde2e8aeb5082
sachinkr0872 <sachinkr0872@gmail.com>
2026-01-18 23:00:56 +0530

    feat: create list with bonus calc

--- FE-02/week-2/project.js
+++ FE-02/week-2/project.js
@@ -1,12 +1,19 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const employees = JSON.parse(fs.readFileSync('./employees.json', 'utf-8'))
+const employees = JSON.parse(fs.readFileSync("./employees.json", "utf-8"));
 
 const sortedBySalary = [...employees].sort((a, b) => {
-    b.salary - a.salary
-})
+  b.salary - a.salary;
+});
 
-const experiencedEmployees = sortedBySalary.filter(emp => {
-    return emp.experience >= 3
-})
+const experiencedEmployees = sortedBySalary.filter((emp) => {
+  return emp.experience >= 3;
+});
+
+const summary = experiencedEmployees.map((emp) => ({
+  name: emp.name,
+  department: emp.department,
+  bonus: emp.salary * 0.1 * emp.experience,
+}));
+
 
