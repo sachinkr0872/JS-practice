794824d4096eba01db2bb93ef289598d23cfe9d2
sachinkr0872 <sachinkr0872@gmail.com>
2026-01-20 23:50:38 +0530

    learn : synchronous single threaded lang

--- FE-02/week-3/depth.js
+++ FE-02/week-3/depth.js
@@ -1,5 +1,11 @@
 // {1} --------- HOW JS works ------------- 
 
+
+// --------- Execution Context --------
+
 // 1st component - Memory or variable environment (key:value , a= 10, fn. ...)
 
 // 2nd component - Code or Thread of Execution (code executed one line at a time )
+
+
+// JavaScript is a synchronous single-threaded language (execute one command at a time and in a specific order one the first line ended then goes to other )
