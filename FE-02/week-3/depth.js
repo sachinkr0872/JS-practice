e2cb6aad49388837ed3fc6acf409e848600ea256
sachinkr0872 <sachinkr0872@gmail.com>
2026-01-20 23:45:02 +0530

    learn: about js components

---
+++ FE-02/week-3/depth.js
@@ -0,0 +1,5 @@
+// {1} --------- HOW JS works ------------- 
+
+// 1st component - Memory or variable environment (key:value , a= 10, fn. ...)
+
+// 2nd component - Code or Thread of Execution (code executed one line at a time )
