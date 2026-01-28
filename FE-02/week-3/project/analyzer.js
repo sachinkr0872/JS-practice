32413de24e9e33a40e68186c7bea14c4cdbc0dc8
sachinkr0872 <sachinkr0872@gmail.com>
2026-01-28 23:19:31 +0530

    feat: add csv parsing code

--- FE-02/week-3/project/analyzer.js
+++ FE-02/week-3/project/analyzer.js
@@ -1,4 +1,18 @@
 const fs = require("fs");
 
-const Data = fs.readFileSync('bank-statements.csv','utf-8');
+const data = fs.readFileSync('bank_statements.csv','utf-8');
 
+
+
+// ------- CSV Parsing --------
+const lines = data.trim().split("\n");
+const headers = lines[0].split(",");
+
+const transactions = lines.slice(1).map(line => {
+  const values = line.split(",");
+  let obj = {};
+  headers.forEach((header, index) => {
+    obj[header.trim()] = values[index].trim();
+  });
+  return obj;
+});
