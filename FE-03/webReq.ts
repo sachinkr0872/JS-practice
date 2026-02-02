bcdaf3a50223126440fa13648202ad062bc8446e
sachinkr0872 <sachinkr0872@gmail.com>
2026-02-02 21:23:42 +0530

    learn: more about axios

---
+++ FE-03/webReq.ts
@@ -0,0 +1,33 @@
+// Type Definations and Axios in Typescript
+
+// Axios -- web request library as like we use fetch
+
+import axios, {AxiosResponse} from "axios";
+
+//first we have to create an interface and define the types of each data and this is very useful in overall journey of TS
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const fetchData = async () => {
+   try {
+      const response: AxiosResponse<Todo> = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
+       console.log("Todo", response.data);
+
+   } catch (error: any) {
+
+     if(axios.isAxiosError(error)) {
+      console.log("Axios Error", error.message);
+         
+          if(error.response){
+            console.log(error.response.status);
+          }
+     }
+
+   }
+}
