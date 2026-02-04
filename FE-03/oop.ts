e5800d16029447244ad6385b30bbc3a496aed974
sachinkr0872 <sachinkr0872@gmail.com>
2026-02-04 21:14:38 +0530

    add : learn about oops concept

---
+++ FE-03/oop.ts
@@ -0,0 +1,24 @@
+// Object Oriented Programming
+
+class Chai {
+  flavour: string;
+  price: number;
+
+  constructor(flavour: string, price: number) {
+    this.flavour = flavour;
+    this.price = price;
+  }
+
+//   constructor(flavour: string, price: number) {
+//     this.flavour = flavour;
+//     console.log(this)
+   
+//   }
+}
+
+const masalaChai = new Chai("Ginger", 20);
+masalaChai.flavour = "masala";
+
+
+
+
