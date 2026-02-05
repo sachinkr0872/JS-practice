eae2febb9485262f910ff047d301a97ad9ac0bab
sachinkr0872 <sachinkr0872@gmail.com>
2026-02-05 23:08:59 +0530

    learn: static members and abstract classes

--- FE-03/oop.ts
+++ FE-03/oop.ts
@@ -12,74 +12,91 @@
 //   constructor(flavour: string, price: number) {
 //     this.flavour = flavour;
 //     console.log(this)
-   
+
 //   }
 // }
 
 // const masalaChai = new Chai("Ginger", 20);
 // masalaChai.flavour = "masala";
 
-
- 
 // ---------------- Access Modifier -------------------
 
 class Chai {
-    public flavor: string = "masala"
+  public flavor: string = "masala";
 
-    private secretIngredients = " cardamom"
+  private secretIngredients = " cardamom";
 
-    reveal() {
-        return this.secretIngredients  // ok
-    }
-
-  
+  reveal() {
+    return this.secretIngredients; // ok
+  }
 }
 
 class Shop {
-      protected shopName = "Chai corner"
+  protected shopName = "Chai corner";
 }
 
-class Branch extends Shop{
-    getName() {
-        return this.shopName  //OK
-    }
+class Branch extends Shop {
+  getName() {
+    return this.shopName; //OK
+  }
 }
-
 
 //  -------  Private
-class Wallet{
-    #balance = 100
+class Wallet {
+  #balance = 100;
 
-    getBalance(){
-        return this.#balance
-    }
+  getBalance() {
+    return this.#balance;
+  }
 }
 
-const w = new Wallet()
+const w = new Wallet();
 
-class Cup{
-   readonly capacity: number = 250
+class Cup {
+  readonly capacity: number = 250;
 
-   constructor(capacity:number){
-    this.capacity = capacity
-   }
+  constructor(capacity: number) {
+    this.capacity = capacity;
+  }
 }
 
-// use of getter and setter(to get and set  the value used in private classes) 
+// use of getter and setter(to get and set  the value used in private classes)
 class ModernChai {
-    private _sugar = 2
+  private _sugar = 2;
 
-    get sugar(){
-        return this._sugar
-    }
+  get sugar() {
+    return this._sugar;
+  }
 
-    set sugar(value:number){
-        if(value>5) throw new Error("Too sweet")
+  set sugar(value: number) {
+    if (value > 5) throw new Error("Too sweet");
 
-            this._sugar = value
-    }
+    this._sugar = value;
+  }
 }
 
-const c = new ModernChai()
-c.sugar = 3
+const c = new ModernChai();
+c.sugar = 3;
 
+// ---------------- Static Members ------------------
+
+class EkChai {
+  static shopName = " Chaicode cafe";
+
+  constructor(public flavor: string) {}
+}
+
+console.log(EkChai.shopName);
+
+// ---------- Abstract Classes ----------
+
+abstract class Drink {
+  abstract make(): void;
+}
+
+
+class MyChai extends Drink {
+  make() {
+    console.log("Brewing Chai");
+  }
+}
