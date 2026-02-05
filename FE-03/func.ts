0840a5983df39f732cb67f1d75544b92c82b1045
sachinkr0872 <sachinkr0872@gmail.com>
2026-02-06 00:11:24 +0530

    learn : functions in ts

---
+++ FE-03/func.ts
@@ -0,0 +1,9 @@
+function makeChai(type: string, cups: number){
+    console.log(`Making ${cups} cups of ${type}`)
+}
+
+makeChai("Masala", 20)
+
+function getChaiPrice(): number {
+      return 200
+}
