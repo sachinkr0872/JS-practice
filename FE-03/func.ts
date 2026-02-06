82144fb1d22837cfe59c073fd5c3e1eca8522bfd
sachinkr0872 <sachinkr0872@gmail.com>
2026-02-06 21:06:50 +0530

    learn: functions in TS

--- FE-03/func.ts
+++ FE-03/func.ts
@@ -1,9 +1,33 @@
-function makeChai(type: string, cups: number){
-    console.log(`Making ${cups} cups of ${type}`)
+function makeChai(type: string, cups: number) {
+  console.log(`Making ${cups} cups of ${type}`);
 }
 
-makeChai("Masala", 20)
+makeChai("Masala", 20);
 
 function getChaiPrice(): number {
-      return 200
+  return 200;
+}
+
+function makeOrder(order: string) {
+  if (!order) return null;
+  return order;
+}
+
+// -------- Logger function(used in databases)
+function logChai(): void {
+  console.log("Chai is ready");
+}
+
+// ----------- Optional and default parameter
+
+// function orderChai(type?: string) {}
+
+function orderChai(type: string = " Masala") {}
+
+function createChai(order: {
+  type: string;
+  sugar: number;
+  size: "small" | "large";
+}): number {
+  return 4;
 }
