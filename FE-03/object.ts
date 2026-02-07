081c4c4b843055d60cc46ae49e30c91a4e0f451f
sachinkr0872 <sachinkr0872@gmail.com>
2026-02-07 23:37:15 +0530

    learn: splitting data types in object

--- FE-03/object.ts
+++ FE-03/object.ts
@@ -25,23 +25,64 @@ tea = {
 //---------  Types  ALiased objects
 
 type Tea = {
-    name: string;
-    price: number;
-    ingredients: string[]
-}
+  name: string;
+  price: number;
+  ingredients: string[];
+};
 
-const adrakChai : Tea = {
-    name : "adrak chai",
-    price: 25,
-    ingredients: ["ginger", "milk", "tea leaves"]
-}
+const adrakChai: Tea = {
+  name: "adrak chai",
+  price: 25,
+  ingredients: ["ginger", "milk", "tea leaves"],
+};
 
-// Structural and duck typing  
+// Structural and duck typing
 
-type Cup = { size: string}
+type Cup = { size: string };
 
-let smallCup : Cup = {size : "200ml"}
+let smallCup: Cup = { size: "200ml" };
 
-let bigCup = {size : "500ml", material: "steel"}
+let bigCup = { size: "500ml", material: "steel" };
 
-smallCup = bigCup
+smallCup = bigCup;
+
+type Brew = { brewTime: number };
+const coffee = { brewTime: 5, beans: "Arabica" };
+const chaiBrew: Brew = coffee;
+
+type User1 = {
+  username: string;
+  password: string;
+};
+
+const u1: User1 = {
+  username: "ChaiCode",
+  password: "123",
+};
+
+// ----------- Splitting Data Types -------------
+
+type Item = { name: string; quantity: number };
+type Address = { street: string; pin: number };
+
+type Order = {
+  id: string;
+  items: Item[];
+  address: Address;
+};
+
+// define a data type in one but use multiple places in object
+
+type Chaii = {
+  name: string;
+  price: number;
+  isHot: boolean;
+};
+
+const updateChai = (updates: Partial<Chaii>) => {
+  console.log("updating chai with", updates);
+};
+
+updateChai({ price: 25 });
+updateChai({ isHot: true });
+updateChai({});
