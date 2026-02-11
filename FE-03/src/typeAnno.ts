efc3f22720c9fc794465c47dd260aac3ab0c8450
sachinkr0872 <sachinkr0872@gmail.com>
2026-02-11 23:35:00 +0530

    learn: type annotation and inference

---
+++ FE-03/src/typeAnno.ts
@@ -0,0 +1,14 @@
+// -------Type Annotation & Inference --------------
+
+let drink = "chai";
+
+// this is type inferencing( already tell or assigned )
+let cups = Math.random() > 0.5 ? 10 : "5";
+
+let channelName = "Chai aur code";
+// ------------------------------------------
+let chaiFlavour: string = "Masala Chai";
+chaiFlavour = "Ginger Tea";
+
+// ----------- Annotation( we have to tell or assign ) -------------
+let chaiOrder: boolean;
