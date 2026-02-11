2019e0c8ef8396bc43421dea6fc93da835383e94
sachinkr0872 <sachinkr0872@gmail.com>
2026-02-11 23:55:12 +0530

    learn about Any

--- FE-03/src/unionAny.ts
+++ FE-03/src/unionAny.ts
@@ -11,3 +11,16 @@ apiRequestStatus = "error";
 let airlineSeat: "aisle" | "window" | "middle" = "aisle";
 airlineSeat = "window";
 
+// ---------- Use of Any and how to avoid it
+const orders = ["12", "20", "24", "40"];
+let currentorder: string | undefined;
+
+for (let order of orders) {
+  if (order === "24") {
+    currentorder = order;
+    break;
+  }
+  currentorder = "11";
+}
+
+console.log(currentorder);
