75735334ae8d11a575018fa78dd4c092e090bf36
sachinkr0872 <sachinkr0872@gmail.com>
2026-02-11 23:51:17 +0530

    learn: Union

---
+++ FE-03/src/unionAny.ts
@@ -0,0 +1,13 @@
+// ---------------- Union & Any -----------------------
+
+// ------- Unions --------
+
+let subs: number | string = "1M";
+
+let apiRequestStatus: "pending" | "success" | "error";
+
+apiRequestStatus = "error";
+
+let airlineSeat: "aisle" | "window" | "middle" = "aisle";
+airlineSeat = "window";
+
